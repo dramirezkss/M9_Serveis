cbf9174310e689f11194607ea978300cff428667
david ramirez <2023_david.ramirez@iticbcn.cat>
2024-09-24 15:11:15 +0200

    version final

--- Rot13.java
+++ Rot13.java
@@ -1,20 +1,58 @@
 import java.util.Scanner;
 
 public class Rot13 {
-    private static final char[] minus = {'a','b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    private static final char[] mayus = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    public xifraRot13(String paraula) {
 
-        for (int i = 0; i < minus.length(); i++) {
+    private final static char[] minus = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private final static char[] mayus = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public static String xifraRot13(String paraula) {
+        String result = "";
+
+        for (int i = 0; i < paraula.length(); i++) {
+            char charActual = paraula.charAt(i);
+
+            for (int j = 0; j < minus.length; j++) {
+                if (charActual == minus[j]) {
+                    int nuevaPosicion;
+                    if (j + 13 < minus.length) {
+                        nuevaPosicion = j + 13;
+                    } else {
+                        nuevaPosicion = j - 13;
+                    }
+                    result = result + minus[nuevaPosicion];
+                    break;
+                }
+            }
+
+            for (int j = 0; j < mayus.length; j++) {
+                if (charActual == mayus[j]) {
+                    int nuevaPosicion;
+                    if (j + 13 < mayus.length) {
+                        nuevaPosicion = j + 13;
+                    } else {
+                        nuevaPosicion = j - 13;
+                    }
+                    result = result + mayus[nuevaPosicion];
+                    break;
+                }
+            }
         }
-
+        return result;
     }
+
+    public static String desxifraRot13(String paraula) {
+        return xifraRot13(paraula);
+    }
+
     public static void main(String[] args) {
-        
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Escribe una letra en minusculas");
+        System.out.println("Escribe una palabra en minusculas o mayusculas");
         String paraula = entrada.nextLine();
-        System.out.println(paraula);
-        xifraRot13("aeiou");
+        String textoCifrado = xifraRot13(paraula);
+        String textoDescifrado = desxifraRot13(paraula);
+
+        System.out.println("Texto original: " + paraula);
+        System.out.println("Texto cifrado: " + textoCifrado);
+        System.out.println("Texto descifrado: " + textoDescifrado);
     }
 }
